bf2646240285661705540fa692df0a901e783853
AntonellaSanti <antonella.santi@hotmail.com>
2023-11-11 10:42:06 -0300

    Antonella 11/11/2023

--- db_init.sql
+++ db_init.sql
@@ -1,2 +1,3 @@
 -- Aqui vamos a indicar todo el codigo SQL que vamos a estar utilizando, a veces se separa en Codigo de Cracion de BBDD y Codigo de carga de datos:
 -- Aqui vamos a indicar todo el codigo SQL que vamos a estar utilizando, a veces se separa en Codigo de Cracion de BBDD y Codigo de carga de datos:
+-- Aqui vamos a indicar todo el codigo SQL que vamos a estar utilizando, a veces se separa en Codigo de Cracion de BBDD y Codigo de carga de datos:
