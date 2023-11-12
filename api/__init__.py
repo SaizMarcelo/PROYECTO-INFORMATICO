5d2120f08097d91ae6bf48d4526c1e4e0d17696f
FedePiscio <69063261+FedePiscio@users.noreply.github.com>
2023-11-12 08:56:55 -0300

    Fede: login OK

--- api/__init__.py
+++ api/__init__.py
@@ -13,15 +13,6 @@ app.config['SECRET_KEY'] = 'app_123'
 def index():
     return render_template('public/index.html')
 
-"""# DATOS USUARIO
-@app.route('/usuarioForm0001')
-def usuarioForm0001():
-    return render_template('public/usuarioForm0001.html')
-
-# DASHBOARD
-@app.route('/inicioForm0001')
-def inicioForm0001():
-    return render_template('public/inicioForm0001.html')"""
 
 # Conectamos las rutas de usuario
 import api.routes.usuario
--- api/routes/rutasHTML.py
+++ api/routes/rutasHTML.py
@@ -4,15 +4,17 @@ from flask_cors import CORS
 from api import app
 
 
-# DATOS USUARIO
+# Redireccionamiento: DATOS USUARIO
 @app.route('/usuarioForm0001')
 def usuarioForm0001():
     return render_template('public/usuarioForm0001.html')
 
-# DASHBOARD
+# Redireccionamiento: INICIO
 @app.route('/inicioForm0001')
 def inicioForm0001():
     return render_template('public/inicioForm0001.html')
+
+
 
 # Conectamos las rutas de usuario
 import api.routes.usuario
