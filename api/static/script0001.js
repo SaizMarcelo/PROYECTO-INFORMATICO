78d5c95777a08d2299aae5c50b39984452e576a8
FedePiscio <69063261+FedePiscio@users.noreply.github.com>
2023-11-21 23:02:02 -0300

    Anto: front-End

--- api/static/script0001.js
+++ api/static/script0001.js
@@ -1,51 +1,52 @@
 
 // Primero se requiere recuperar la informacion, luego realizar un Fetch() a la ruta que corresponda.
 function iniciarSesion(){
-// Recuperamos el username: lo consultaremos al documento por su Id del elemento.
-const username = document.getElementById('in-username').value;
-// Recuperamos la contraseña lo consultaremos al documento por su Id del elemento.
-const password = document.getElementById('in-password').value;
-// Creamos la estructura del Request: Consulta:
-const requestOption = {
-    // Metodo a utilizar:
-    method: "POST",
-    // Creamos la misma estructura que debe estar estructurado el HEADER de la reuquest:
-    headers: {
-        'Content-Type' : 'aplicattion/json',
-        // btoa codifica en base64 para que el backend la identifique.
-        'Authorization' : 'Basic ' + btoa(username + ":" + password)
-    }
-}
-// Realizamos el Fetch()
-fetch('http://127.0.0.1:4500/login', requestOption)
-// Como es una promesa vamos a trabajarlo con el then - cach:
-.then(
-    res => { return res.json()}
-    )
-.then(
-    // Si llegamos a este punto hacemos un control.log
-    resp => {
-        console.log(resp)
-        // Inicio de sesion correcta, al tener Token:
-        if(resp.Token){
-            // Guardamos la informacion del token en un LOCALSTORAGE del cliente:
-            localStorage.setItem('token', resp.Token)
-            localStorage.setItem('username', resp.Username)
-            localStorage.setItem('id', resp.id)
-            // Enviamos al spam message del html la respuesta obtenida aqui:
-            document.getElementById("message").innerHTML = 'Bienvenido ' + resp.Username
-            // Esta es la forma para movernos a otro documento HTML:
-            // Importante utilizar comillas a la izquierda:
-            window.location.href = '/dashboardForm0001';
-            
+    // Recuperamos el username: lo consultaremos al documento por su Id del elemento.
+    const username = document.getElementById('in-username').value;
+    // Recuperamos la contraseña lo consultaremos al documento por su Id del elemento.
+    const password = document.getElementById('in-password').value;
+    // Creamos la estructura del Request: Consulta:
+    const requestOption = {
+        // Metodo a utilizar:
+        method: "POST",
+        // Creamos la misma estructura que debe estar estructurado el HEADER de la reuquest:
+        headers: {
+            'Content-Type' : 'aplicattion/json',
+            // btoa codifica en base64 para que el backend la identifique.
+            'Authorization' : 'Basic ' + btoa(username + ":" + password)
         }
-        // De lo contrario buscamos del documento el id:span mostramos el codigo.
-        else{
-            // Le pasamos la respuesta del servidor:
-            document.getElementById("message").innerHTML = resp.Messenfe;
+    }
+    
+    // Realizamos el Fetch() - LOGIN
+    fetch('http://127.0.0.1:4500/login', requestOption)
+    // Como es una promesa vamos a trabajarlo con el then - cach:
+    .then(
+        res => { return res.json()}
+        )
+    .then(
+        // Si llegamos a este punto hacemos un control.log
+        resp => {
+            console.log(resp)
+            // Inicio de sesion correcta, al tener Token:
+            if(resp.Token){
+                // Guardamos la informacion del token en un LOCALSTORAGE del cliente:
+                localStorage.setItem('token', resp.Token)
+                localStorage.setItem('username', resp.Username)
+                localStorage.setItem('id', resp.id)
+                // Enviamos al spam message del html la respuesta obtenida aqui:
+                document.getElementById("message").innerHTML = 'Bienvenido ' + resp.Username
+                // Esta es la forma para movernos a otro documento HTML:
+                // Importante utilizar comillas a la izquierda:
+                window.location.href = '/dashboardForm0001';
+                
+            }
+            // De lo contrario buscamos del documento el id:span mostramos el codigo.
+            else{
+                // Le pasamos la respuesta del servidor:
+                document.getElementById("message").innerHTML = resp.Messenfe;
+            }
+        
         }
     
+    )
     }
-
-)
-}
--- api/static/script0002.js
+++ api/static/script0002.js
@@ -11,6 +11,7 @@ window.onload = function(){
         document.getElementById("token").innerHTML = token
         // Mensaje id:
         document.getElementById("id").innerHTML = id
+
     }
     // Si el token no es correcto tenemos que limitar la permanencia en la pagina:
     else{
@@ -19,11 +20,12 @@ window.onload = function(){
     }
 }
 
-
+// FUNCION CARGAR CLIENTES.
 function cargarClientes(){
     // Obtenemos la ID del Usuario:
     const id = localStorage.getItem('id');
     const token = localStorage.getItem('token');
+
 
     // Creamos el objeto Request para crear el cliente: JSON
     const requestOption = {
@@ -35,16 +37,255 @@ function cargarClientes(){
         }
     }
 
-    fetch(`http://127.0.0.1:4500/user/${id}/client`, requestOption)
+    fetch(`http://127.0.0.1:4500/users/${id}/client`, requestOption)
     .then(
-        resp  => {return resp.json()}
+        resp  => {
+            
+            return resp.json()
+        }
+
     )
     .then(
         resp => {
+            // De la consulta vamos a conulstar cuantos registros tiene la cunsulta realizada:
+            localStorage.setItem('consultaPuente', resp)
             console.log(resp)
             console.log(id)
             console.log(token)
+            var contador = 0
+            var contenedorDinamico = document.getElementById("contenedorDinamico");
+            contenedorDinamico.innerHTML="";
+
+            var buscador ='<div><input type="text" id="searchInput" onkeyup="buscarEnTabla()" placeholder="Buscar por apellido..."></div>'
+            var tabla = buscador+'<table id="myTable" class="myTable">';
+            tabla += `<tr><td>ID</td><td>DNI</td><td>APELLIDO</td><td>NOMBRE</td><td></td></tr>`
+            for (let key in resp){
+                console.log(key)
+                console.log(resp[contador]["name"])
+                console.log(resp[contador]["surname"])
+                
+                tabla += `<tr><td>${resp[contador]["id"]}</td><td>NunDni</td><td>${resp[contador]["surname"]}</td><td>${resp[contador]["name"]}</td><td><button onclick="consultarIdBotonVer(this),buscarDatosCliente(),openModal()">Ver</button></td></tr>`
+
+                contador += 1
+            }
+            tabla += "</table>";
+            contenedorDinamico.innerHTML = tabla;
         }
     )
-    
 }
+
+// Creamos una funcion para obtener el ID del cliente que se selecciona desde la tabla:
+function consultarIdBotonVer(boton){
+        //Nos Posicionamos en el nodo td-tr de esta forma:
+        var fila = boton.parentNode.parentNode; 
+        // De los 4 td nos posicionamos en el primero que tiene el dato requerido.
+        var datoPrimeraColumna = fila.getElementsByTagName('td')[0].innerText; // Obtener el valor de la primera columna
+        localStorage.setItem("idClienteVer", datoPrimeraColumna);
+}
+
+// Consultamos el Cliente seleccionado en la tabla: boton ver.
+function buscarDatosCliente(){
+    // Obtenemos la ID del Usuario:
+    const id = localStorage.getItem('id');
+    const token = localStorage.getItem('token');
+    const idClienteVer = localStorage.getItem('idClienteVer')
+
+    // Creamos el objeto Request para crear el cliente: JSON
+    const requestOption = {
+        method : 'GET',
+        headers: {
+            'Content-Type': 'aplication/json',
+            'x-access-token': token,
+            'user-id': id,
+        }
+    }
+
+    fetch(`http://127.0.0.1:4500/users/${id}/client/${idClienteVer}`, requestOption)
+    .then(
+        resp  => {
+            
+            return resp.json()
+        }
+        
+    )
+    .then(
+        resp => {
+            // De la consulta vamos a conulstar cuantos registros tiene la cunsulta realizada:
+            console.log(resp)
+            console.log(id)
+            console.log(token)
+            // Enviamos los datos para cargar el fomulario:
+            deshabilitarInput("address")
+            document.getElementById("address").value = resp["address"]
+            deshabilitarInput("email")
+            document.getElementById("email").value = resp["email"]
+            deshabilitarInput("idCliente")
+            document.getElementById("idCliente").value = resp["id"]
+            deshabilitarInput("name")
+            document.getElementById("name").value = resp["name"]
+            deshabilitarInput("phone_number")
+            document.getElementById("phone_number").value = resp["phone_number"]
+            deshabilitarInput("surname")
+            document.getElementById("surname").value = resp["surname"]
+            deshabilitarInput("user_id")
+            document.getElementById("user_id").value = resp["user_id"]
+            deshabilitarInput("visibility")
+            document.getElementById("visibility").value = resp["visibility"]
+        }
+    )
+}
+// Funcion PUT - Modificar Datos del Cliente:
+function modificarDatosClient(){
+    // Obtenemos la ID del Usuario:
+    const id = localStorage.getItem('id');
+    const token = localStorage.getItem('token');
+    const idClienteVer = localStorage.getItem('idClienteVer');
+    
+    // Creamos el objeto Request para modificar los datos del cliente: JSON
+    const requestOption = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': token,
+            'user-id': id,
+        },
+        body: JSON.stringify({
+            "name": document.getElementById("name").value,
+            "surname": document.getElementById("surname").value,
+            "email": document.getElementById("email").value,
+            "address": document.getElementById("address").value,
+            "phone_number": document.getElementById("phone_number").value
+        })
+    }
+    
+    fetch(`http://127.0.0.1:4500/users/${id}/client/${idClienteVer}`, requestOption)
+    .then(resp => resp.json())
+    .then(resp => {
+        // Manejar la respuesta de la consulta
+        console.log(resp);
+        console.log(id);
+        console.log(token);
+        // Actualizamos la lista de clientes:
+        cargarClientes();
+    })
+    .catch(error => {
+        // Manejar cualquier error que pueda ocurrir durante la solicitud
+        console.error('Error:', error);
+    });
+}
+
+// Funcion PUT - BORRADO LOGICO:
+function borradoLogicoCliente(){
+    // Obtenemos la ID del Usuario:
+    const id = localStorage.getItem('id');
+    const token = localStorage.getItem('token');
+    const idClienteVer = localStorage.getItem('idClienteVer');
+    
+    // Creamos el objeto Request para modificar los datos del cliente: JSON
+    const requestOption = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': token,
+            'user-id': id,
+        },
+    }
+    
+    fetch(`http://127.0.0.1:4500/users/${id}/client/${idClienteVer}`, requestOption)
+    .then(resp => {
+        if (resp.ok){
+            cargarClientes();
+            document.getElementById("clienteEliminado").innerHTML = 'El cliente fue eliminado definitivamente'
+            deshabilitarBoton("guardarCambios")
+            deshabilitarBoton("botonEditarCliente")
+            deshabilitarBoton("borrarCliente")
+        } else {
+            document.getElementById("clienteEliminado").innerHTML = 'El borrado no tuvo exito'
+        }
+    })
+    .catch(error => {
+        // Manejar cualquier error que pueda ocurrir durante la solicitud
+        console.error('Error:', error);
+    });
+}
+
+ // Buscar apellido en tabla.
+function buscarEnTabla() {
+    // Declara variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    table = document.getElementById('myTable');
+    tr = table.getElementsByTagName('tr');
+  
+    // Recorre todas las filas y oculta las que no coinciden con la búsqueda
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName('td')[2]; // Busca en la segunda columna: apellido
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = '';
+        } else {
+          tr[i].style.display = 'none';
+        }
+      }
+    }
+  }
+
+// Función para mostrar el modal
+function openModal() {
+    document.getElementById('myModal').style.display = 'block';
+    deshabilitarBoton("guardarCambios")
+    habilitarBoton("botonEditarCliente")
+    habilitarBoton("borrarCliente")
+    document.getElementById("clienteEliminado").innerHTML = '♠'
+}
+
+// Función para cerrar el modal
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+// Deshabilitar Input
+function deshabilitarInput(nombre) {
+    document.getElementById(nombre).disabled = true; // Deshabilita el input
+  }
+
+// Habilitar Input
+function habilitarInput(nombre) {
+    document.getElementById(nombre).disabled = false; // Habilita el input
+  }
+
+function botonEditarCliente(){
+    // Habilitamos los imputo a modificar:
+    habilitarInput("address")
+    habilitarInput("email")
+    habilitarInput("name")
+    habilitarInput("phone_number")
+    habilitarInput("surname")
+    deshabilitarBoton("botonEditarCliente")
+    habilitarBoton("guardarCambios")
+}
+
+function botonGuardarCambios(){
+    deshabilitarInput("address")
+    deshabilitarInput("email")
+    deshabilitarInput("idCliente")
+    deshabilitarInput("name")
+    deshabilitarInput("phone_number")
+    deshabilitarInput("surname")
+    deshabilitarInput("user_id")
+    deshabilitarInput("visibility")
+    habilitarBoton("botonEditarCliente")
+    deshabilitarBoton("guardarCambios")
+}
+
+// Deshabilitar Boton
+function deshabilitarBoton(nombre) {
+    document.getElementById(nombre).disabled = true; // Deshabilita el botón
+  }
+
+// Habilitar Boton
+function habilitarBoton(nombre) {
+    document.getElementById(nombre).disabled = false; // Habilita el botón
+  }
