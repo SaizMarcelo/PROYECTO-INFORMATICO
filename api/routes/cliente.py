8c658904da978bb35114cec85ad81dd214db04e7
FedePiscio <69063261+FedePiscio@users.noreply.github.com>
2023-11-22 20:21:32 -0300

    BackEnd Cliente-Usuarios

--- api/routes/cliente.py
+++ api/routes/cliente.py
@@ -2,30 +2,21 @@ from api import app # importamos el objeto app
 from api.models.cliente import Client # importamos clase cliente
 from flask import request, jsonify
 from api.utils import token_required, client_resource, user_resource
-from api.db.db import mysql
 
 
 # CREATE
 @app.route('/users/<int:user_id>/client', methods = ['POST'] )
+@token_required
+@user_resource
 def create_client(user_id):
-
-    name = request.get_json()["name"]
-    surname = request.get_json()["surname"]
-    email = request.get_json()["email"]
-    address = request.get_json()["address"]
-    phone_number = request.get_json()["phone_number"]
+    data = request.get_json()
+    data["user_id"] = user_id
+    try:
+        new_client = Client.create_client(data)
+        return jsonify( new_client ), 201
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
     
-    cur = mysql.connection.cursor()
-    cur.execute('INSERT INTO client (user_id, name, surname, email, address, phone_number, visibility) VALUES (%s, %s, %s, %s, %s, %s, %s)', (user_id, name, surname, email, address, phone_number, True))
-    mysql.connection.commit()
-    cur.execute('SELECT LAST_INSERT_ID()')
-    row = cur.fetchone()
- 
-    id = row[0]
-    return jsonify({"id": id, "user_id": user_id, "name": name, "surname": surname, "email": email, "address": address, "phone_number": phone_number})
-
-
-
 
 # READ
 # Ruta: Get de cliente a traves del ID:
@@ -33,42 +24,25 @@ def create_client(user_id):
 @token_required
 @user_resource
 @client_resource
-def get_client_by_id(user_id, client_id):
-    """ Acceso a BBDD"""
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM client WHERE id= %s AND user_id= %s AND visibility = %s', (client_id, user_id, True))
-    data = cur.fetchall()
-
-    if cur.rowcount > 0:
-        objtClient = Client(data[0])
-        return jsonify( objtClient.to_json() )
-    return jsonify( {"messege": "id not found"}), 404
+def get_client_by_id(client_id, user_id):
+    
+    try:
+        client = Client.get_client_by_id(client_id)
+        return jsonify( client ), 201
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
+   
 
 # RUTA: consulta todos los clientes de un usuario:
 @app.route('/users/<int:user_id>/client', methods = ['GET'] )
 @token_required
 @user_resource
 def get_all_clients_by_user_id(user_id):
-    # Creamos un cursor:
-    cur = mysql.connection.cursor()
-
-    # Realizamos una consulta SQL:
-    cur.execute('SELECT * FROM client WHERE user_id = %s AND visibility = %s', (user_id, True))
-    
-    # Esperamos mas de un registro:
-    data = cur.fetchall()
-
-    # Creamos una lista vacia:
-    clientList = []
-  
-    # Creamos un bucle for: por cada fila: 
-    for row in data:
-        # Creamos un objeto Cliente:
-        objClient = Client(row)
-        # Agregamos un elemento json a la lista:
-        clientList.append(objClient.to_json())
-    # Retornamos la lista de elementos json:
-    return jsonify(clientList)
+    try:
+        clients = Client.get_all_client(user_id)
+        return jsonify( clients ), 201
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
 
 
 
@@ -77,24 +51,14 @@ def get_all_clients_by_user_id(user_id):
 @token_required
 @user_resource
 @client_resource
-def update_client(user_id, client_id):
-    
-    name = request.get_json()["name"]
-    surname = request.get_json()["surname"]
-    email = request.get_json()["email"]
-    address = request.get_json()["address"]
-    phone_number = request.get_json()["phone_number"]
-
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM client WHERE id=%s AND user_id=%s AND visibility = %s', (client_id, user_id, True))
-
-    if cur.rowcount > 0:
-        cur.execute('UPDATE client SET name = %s, surname = %s, email = %s, address = %s, phone_number = %s WHERE id = %s', (name, surname, email, address, phone_number, client_id))
-    
-        mysql.connection.commit()
-        return jsonify({"id": client_id, "name": name, "surname": surname, "email": email, "address": address, "phone_number": phone_number})
-    
-    return jsonify( {"messege": "id not found"}), 404
+def update_client(client_id, user_id):
+    data = request.get_json()
+    data["user_id"] = user_id
+    try:
+        client = Client.update_client_by_id(client_id, data)
+        return jsonify( client ), 201
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
 
 
 
@@ -104,12 +68,8 @@ def update_client(user_id, client_id):
 @user_resource
 @client_resource
 def remove_client(client_id, user_id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM client WHERE id=%s AND user_id = %s AND visibility = %s', (client_id, user_id, True))
-    if cur.rowcount > 0:
-        cur.execute('UPDATE client SET visibility = %s WHERE id = %s', (False, client_id))
-    
-        mysql.connection.commit()
-        return jsonify({"message": "deleted", "id": client_id})
-    
-    return jsonify( {"messege": "id not found"}), 404
+    try:
+        client = Client.remove_client_by_id(client_id)
+        return jsonify( client ), 201
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
--- api/routes/usuario.py
+++ api/routes/usuario.py
@@ -3,85 +3,47 @@ from flask import request, jsonify
 from api.db.db import mysql
 from api.models.usuario import User
 from api.utils import token_required, user_resource
-import jwt
-import datetime as dt
+
 
 
 # Login
 @app.route('/login', methods = ['POST'] )
 def login():
     auth = request.authorization
+    try:
+        log = User.login(auth)
+        return jsonify(log)
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
 
-    """ Control: existen valores para la autenticacion?"""
-    if not auth or not auth.username or not auth.password:
-        return jsonify({"Messenge": "Login No Autorizado"}), 401 # existen librerias que facilitan el manejo de errores
-
-    """ Control: existen y coincide el usuario en la BD?"""
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users WHERE username = %s AND password = %s", (auth.username, auth.password))
-    # Esperamos recibir una fila: devuelve 1ero en fila osea 1.
-    row = cur.fetchone()
-
-    # si la consulta no devuelve filas:
-    if not row:
-        return jsonify({"Messenge": "Login No Autorizado"}), 401 # existen librerias que facilitan el manejo de errores
-
-    """ El usuario existe en la BD y coincide su contrseña"""
-    # Creamos una variable codificada:
-    token = jwt.encode({
-        'id': row[0],
-        'exp': dt.datetime.utcnow() + dt.timedelta(minutes=100)   # tiempo de exporar el password. Tiempo para que el usurio vuelva a iniciar sesion. 
-    }, app.config['SECRET_KEY'])
     
-    return jsonify({"Token": token, "Username": auth.username, "id": row[0]})
-
 # CREATE
 
 @app.route('/signup', methods = ['POST'] )
 def crate_user():
-
-    username = request.get_json()["username"]
-    password = request.get_json()["password"]
-    name = request.get_json()["name"]
-    surname = request.get_json()["surname"]
-    email = request.get_json()["email"]
-    address = request.get_json()["address"]
-    phone_number = request.get_json()["phone_number"]
- 
-
-    cur = mysql.connection.cursor()
-    cur.execute('INSERT INTO users (username, password, name, surname, email, address, phone_number, visibility) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (username, password, name, surname, email, address, phone_number, True))
-    mysql.connection.commit()
-    cur.execute('SELECT LAST_INSERT_ID()')
-    row = cur.fetchone()
- 
-    id = row[0]
-    return jsonify({"id": id, "username": username, "password": password, "name": name, "surname": surname, "email": email, "address": address, "phone_number": phone_number})
+    data = request.get_json()
+    try:
+        new_user = User.create_user(data)
+        return jsonify( new_user ), 201
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
 
 # READ
 @app.route('/users/<int:id>', methods = ['GET'] )
 def get_user_by_id(id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM users WHERE id=%s AND visibility =%s', (id, True))
-    data = cur.fetchall()
-    if cur.rowcount > 0:
-        objUser = User(data[0])
-        if objUser._visibility:
-
-            return jsonify(objUser.to_json())
-    return jsonify( {"messege": "id not found"}), 404
+    try:
+        user = User.get_user_by_id(id)
+        return jsonify( user ), 201
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
 
 @app.route('/users', methods = ['GET'] )
 def get_all_user():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM users')
-    data = cur.fetchall()
-    usersList = []
-    for row in data:
-        objUser = User(row)
-        if objUser._visibility:
-            usersList.append(objUser.to_json())
-    return jsonify(usersList)
+    try:
+        user = User.get_all_users()
+        return jsonify( user ), 201
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
 
 # UPDATE
 @app.route('/users/<int:id>', methods = ['PUT'] )
@@ -89,32 +51,22 @@ def get_all_user():
 @user_resource
 def update_user(id):
     
-    username = request.get_json()["username"]
-    password = request.get_json()["password"]
-    name = request.get_json()["name"]
-    surname = request.get_json()["surname"]
-    email = request.get_json()["email"]
-    address = request.get_json()["address"]
-    phone_number = request.get_json()["phone_number"]
-
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM users WHERE id=%s', (id,))
-    if cur.rowcount > 0:
-        cur.execute('UPDATE users SET username = %s, password = %s, name = %s, surname = %s, email = %s, address = %s, phone_number = %s  WHERE id=%s', (username, password, name, surname, email, address, phone_number, id))
-        
-        mysql.connection.commit()
-        return jsonify({"id": id, "username": username, "password": password, "name": name, "surname": surname, "email": email, "address": address, "phone_number": phone_number})
-    return jsonify( {"messege": "id not found"}), 404
+    data = request.get_json()
+    data["user_id"] = id
+    try:
+        user = User.update_user(data)
+        return jsonify( user ), 201
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
+    
 
 # DELETE
 @app.route('/users/<int:id>', methods = ['DELETE'] )
 @token_required
 @user_resource
 def remove_user(id):
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM users WHERE id=%s AND visibility =%s', (id, True))
-    if cur.rowcount > 0:
-        cur.execute('UPDATE users SET visibility=%s WHERE id=%s', (False, id))
-        mysql.connection.commit()
-        return jsonify({"message": "deleted", "id": id})
-    return jsonify( {"messege": "id not found"}), 404
+    try:
+        delete = User.delete_user(id)
+        return delete
+    except Exception as e:
+        return jsonify( {"message": e.args[0]} ), 400
